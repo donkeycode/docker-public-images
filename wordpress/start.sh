7675aea8b1d840a4bf7c1034508d9b4ea5aa0375
Cedric LOMBARDOT <cedric@donkeycode.com>
2020-12-15 17:58:27 +0100

    Config

--- wordpress/start.sh
+++ wordpress/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 php-fpm &
+crontab /var/www/crontab # Start cron
 envsubst < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf && cat /etc/nginx/conf.d/default.conf && nginx
