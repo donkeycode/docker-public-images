24b4c95fed55312c90e7eab6013f7acca6763bb9
Cedric LOMBARDOT <cedric@donkeycode.com>
2021-04-14 18:31:59 +0200

    Fix sh

--- php/replace-env-file.sh
+++ php/replace-env-file.sh
@@ -2,13 +2,20 @@
 function replace_env() {
   local input="$(< /dev/stdin)"
   local file="$(echo $input | cut -d "=" -f 2)"
+  if [ ! -f "$file" ]; then
+    return
+  fi
+
   local file_content=$(< $file)
   local new_variable="$(echo $input | cut -d "=" -f 1 | sed 's/_SECRET_FILE//')"
 
-  echo "export $new_variable=\"$file_content\""
+  if [ ! -z "$var" ]
+  then
+      echo "export $new_variable=\"$file_content\""
+  fi
 }
 
-echo "" > tbs
+echo "export PARSED=yes"  > tbs
 
 printenv | grep _SECRET_FILE= | replace_env >> tbs
 
