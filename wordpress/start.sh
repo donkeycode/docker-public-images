c2aaba1df5b817c4e31902b903ff4baae09d3865
Cedric LOMBARDOT <cedric@donkeycode.com>
2021-04-20 15:30:12 +0200

    Env with wordpress

--- wordpress/start.sh
+++ wordpress/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /usr/local/bin/replace-env-file.sh
 php-fpm &
 crontab /var/www/crontab # Start cron
 envsubst < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf && cat /etc/nginx/conf.d/default.conf && nginx
