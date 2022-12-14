1f6c0228dba3bcef1b6af4076fddc3cf0bdfb7b4
Cedric LOMBARDOT <cedric@donkeycode.com>
2022-12-14 07:01:34 +0100

    PHP 8.2

--- newrelic-logstash-rancher/start.sh
+++ newrelic-logstash-rancher/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /usr/local/bin/replace-env-file.sh
 envsubst < /usr/share/logstash/config/logstash.conf.template > /usr/share/logstash/config/logstash.conf && cat /usr/share/logstash/config/logstash.conf
-
+cp /usr/share/logstash/config/logstash.conf /usr/share/logstash/pipeline/logstash.conf
+logstash
