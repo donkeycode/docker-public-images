17a53cc198be7ddaa75a4c3c39c022a7120140b6
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2023-03-30 00:41:36 +0200

    FIx caddy

--- php-caddy-symfony/start.sh
+++ php-caddy-symfony/start.sh
@@ -3,4 +3,5 @@ source /usr/local/bin/replace-env-file.sh
 envsubst < /etc/Caddyfile.template > /etc/Caddyfile
 caddy fmt --overwrite /etc/Caddyfile
 cat /etc/Caddyfile
+php-fpm &
 /usr/sbin/caddy run --config /etc/Caddyfile
