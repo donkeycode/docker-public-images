80a4f7816651446c0df759381e9c3332501f27bc
Cédric LOMBARDOT <cedric.lombardot@gmail.com>
2025-05-22 14:46:34 +0200

    Add dagger

--- dagger/ci_watch.sh
+++ dagger/ci_watch.sh
@@ -1,22 +1,50 @@
 #!/bin/bash
 
-
-REPO_DIR="/tmp/ci-repo"
-LAST_COMMIT_FILE="/tmp/last_commit.txt"
+LAST_COMMIT_FILE="/ci/cache/last_commit.txt"
 
 mkdir -p "$REPO_DIR"
 cd "$REPO_DIR" || exit 1
 
+mkdir -p "$SSH_DIR"
+KEY_FILE="$SSH_DIR/id_ed25519"
+
+if [ ! -f "$KEY_FILE" ]; then
+    echo "🔐 Generate auth key..."
+    ssh-keygen -t ed25519 -f "$KEY_FILE" -q -N ""
+    echo ""
+    echo "📎 Add this key in your github :"
+    echo "---------------------------------------------------"
+    cat "$KEY_FILE.pub"
+    echo "---------------------------------------------------"
+    echo ""
+fi
+
+export GIT_SSH_COMMAND="ssh -i $KEY_FILE -o StrictHostKeyChecking=no"
+
+pip install --no-cache-dir -r /ci/pipeline/requirements.txt
+
 while true; do
-    git clone --quiet --branch "$BRANCH" --depth 1 "$REPO_URL" . 2>/dev/null || git pull origin "$BRANCH"
+    if [ ! -d "$REPO_DIR/.git" ]; then
+        echo "📦 Try clone repo..."
+        rm -rf "$REPO_DIR"/* 2>/dev/null
+        git clone --quiet --branch "$BRANCH" "$REPO_URL" "$REPO_DIR" || {
+            echo "❌ Fail have u allowed the ssh key ?"
+            sleep 60
+            continue
+        }
+    else
+        git fetch origin "$BRANCH"
+        git reset --hard "origin/$BRANCH"
+    fi
+
     LATEST_COMMIT=$(git rev-parse HEAD)
 
     if [ ! -f "$LAST_COMMIT_FILE" ] || [ "$LATEST_COMMIT" != "$(cat $LAST_COMMIT_FILE)" ]; then
         echo "$LATEST_COMMIT" > "$LAST_COMMIT_FILE"
-        echo "🛠 New commit detected, Dagger pipeline executed..."
-        dagger run python3 ci_pipeline.py
+        echo "🛠 New commit run dagger..."
+        dagger run python3 /ci/pipeline/pipeline.py
     else
-        echo "✅ No change detected."
+        echo "✅ No changes found"
     fi
 
     sleep 60
