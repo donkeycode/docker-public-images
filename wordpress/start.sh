46dafc5d5508df583ff1b02d9713cd91b2afcc86
Cédric LOMBARDOT <cedric.lombardot@gmail.com>
2025-07-02 18:52:26 +0200

    Fix wp

--- wordpress/start.sh
+++ wordpress/start.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 source /usr/local/bin/replace-env-file.sh
 php-fpm &
-crontab /var/www/crontab # Start cron
-envsubst < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf && cat /etc/nginx/conf.d/default.conf && nginx
+crontab /var/www/crontab
+envsubst < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+cat /etc/nginx/conf.d/default.conf
+nginx
