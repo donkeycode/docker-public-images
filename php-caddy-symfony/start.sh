8aa78090f666d6234837a2428f090e4d6eafc087
Cédric LOMBARDOT <cedric.lombardot@gmail.com>
2025-07-02 18:15:26 +0200

    TEMPLATE CSP OR NO CSP

--- php-caddy-symfony/start.sh
+++ php-caddy-symfony/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /usr/local/bin/replace-env-file.sh
-envsubst < /etc/Caddyfile.template > /etc/Caddyfile
+envsubst < /etc/$TEMPLATE > /etc/Caddyfile
 caddy fmt --overwrite /etc/Caddyfile
 cat /etc/Caddyfile
 php-fpm &
