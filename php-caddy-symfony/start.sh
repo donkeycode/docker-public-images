92a7df9842d9149c0be174ec403d33f0e83cf0cc
Cédric LOMBARDOT <cedric.lombardot@gmail.com>
2025-08-08 11:09:15 +0200

    Cache

--- php-caddy-symfony/start.sh
+++ php-caddy-symfony/start.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 source /usr/local/bin/replace-env-file.sh
-
-if [ "$CACHE" = "cache" ]; then
-    export CACHE="$(cat /etc/souin.conf)"
-elif [ "$CACHE" = "" ]; then
-    export CACHE=""
-fi
-
 envsubst < /etc/$TEMPLATE > /etc/Caddyfile
 caddy fmt --overwrite /etc/Caddyfile
 cat /etc/Caddyfile
