aedbf8e126879245794f68f923dd797ae649cdd1
Cédric LOMBARDOT <cedric.lombardot@gmail.com>
2026-04-20 18:09:04 +0200

    Init postiz

---
+++ postiz/replace-env-file.sh
@@ -0,0 +1,26 @@
+# Get all env variables
+function replace_env() {
+  local input="$1"
+  local file="$(echo $input | cut -d "=" -f 2)"
+
+  echo "# Parse input ${input}"
+  echo "# File is ${file}"
+
+  if [ ! -f "$file" ]; then
+    return
+  fi
+
+  local file_content=$(cat $file)
+  local new_variable="$(echo $input | cut -d "=" -f 1 | sed 's/_SECRET_FILE//')"
+
+  echo "export $new_variable=\"$file_content\""
+}
+
+echo "FILE_ENV_SECRET_LOADED=yes" > ~/environment
+
+for i in $(printenv | grep _SECRET_FILE)
+do
+  replace_env $i >> ~/environment
+done
+
+source ~/environment
